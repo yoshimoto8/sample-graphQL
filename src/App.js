7418c6f74b15529e5def03b0b11cec54700e333d
吉本光輝 <yoyoyo427435@gmail.com>
2018-11-25 12:39:29 +0900

    applo-client setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import client from "./client";
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-console.log(GITHUB_TOKEN);
+const ME = gql`
+  query me {
+    user(login: "yoshimoto8") {
+      name
+      avatarUrl
+    }
+  }
+`;
+
 class App extends Component {
   render() {
-    return <div className="App">hello graph ql</div>;
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">hello graph ql</div>
+        <Query query={ME}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `${error}`;
+            return <div>{data.user.name}</div>;
+          }}
+        </Query>
+      </ApolloProvider>
+    );
   }
 }
 
