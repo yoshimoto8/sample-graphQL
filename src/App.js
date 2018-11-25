abb32a27855c85aaa7aab17d14fd00f479cb6092
吉本光輝 <yoyoyo427435@gmail.com>
2018-11-25 15:09:35 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
 import React, { Component } from "react";
 import { ApolloProvider } from "react-apollo";
-import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import client from "./client";
+import { ME, SEARCH_REPOSITORIES } from "./graphql";
+import { list } from "postcss";
 
-const ME = gql`
-  query me {
-    user(login: "yoshimoto8") {
-      name
-      avatarUrl
-    }
-  }
-`;
-
+const PER_PAGE = 5;
+const DEFAULT_STATE = {
+  first: PER_PAGE,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア"
+};
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = DEFAULT_STATE;
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      ...DEFAULT_STATE,
+      query: event.target.value
+    });
+  }
+
+  goNext(search) {
+    this.setState({
+      first: PER_PAGE,
+      after: search.pageInfo.endCursor,
+      last: null,
+      before: null
+    });
+  }
+
   render() {
+    const { query, first, after, before, last } = this.state;
+    console.log({ query });
     return (
       <ApolloProvider client={client}>
-        <div className="App">hello graph ql</div>
-        <Query query={ME}>
+        <from>
+          <input value={query} onChange={this.handleChange} />
+        </from>
+        <Query
+          query={SEARCH_REPOSITORIES}
+          variables={{ query, first, after, before, last }}
+        >
           {({ loading, error, data }) => {
             if (loading) return "Loading...";
             if (error) return `${error}`;
-            return <div>{data.user.name}</div>;
+            const search = data.search;
+            const repositoryCount = search.repositoryCount;
+            const repositoryCountoryUnit =
+              repositoryCount === 1 ? "Repository" : "Repositories";
+
+            const title = `GitHub Repositrories Search Results - ${repositoryCount} ${repositoryCountoryUnit}`;
+            return (
+              <React.Fragment>
+                <h2>{title}</h2>
+                <ul>
+                  {search.edges.map((edge, index) => {
+                    const node = edge.node;
+                    return (
+                      <li key={index}>
+                        <a href={node.url} target="_blank">
+                          {node.name}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+                {search.pageInfo.hasNextPage === true ? (
+                  <button onClick={this.goNext.bind(this, search)}>Next</button>
+                ) : null}
+              </React.Fragment>
+            );
           }}
         </Query>
       </ApolloProvider>
---
+++ src/graphql.js
@@ -0,0 +1,51 @@
+import gql from "graphql-tag";
+
+export const ME = gql`
+  query me {
+    user(login: "yoshimoto8") {
+      name
+      avatarUrl
+    }
+  }
+`;
+
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories(
+    $first: Int
+    $after: String
+    $last: Int
+    $before: String
+    $query: String!
+  ) {
+    search(
+      first: $first
+      after: $after
+      last: $last
+      before: $before
+      query: $query
+      type: REPOSITORY
+    ) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
