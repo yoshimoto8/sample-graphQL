159efc078980a8952cc487cc9764d16a44fb90ac
吉本光輝 <>
2018-11-25 12:03:48 +0900

    token追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+console.log(GITHUB_TOKEN);
 class App extends Component {
   render() {
     return <div className="App">hello graph ql</div>;
